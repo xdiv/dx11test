755840b3322bfcd386f648159846222e7bb664bd
xdiv <agotovtas@gmail.lt>
2015-06-01 20:12:43 +0300

    clean

--- dx11test/dx11test/ShaderBase.h
+++ dx11test/dx11test/ShaderBase.h
@@ -13,13 +13,6 @@ using namespace std;
 #pragma comment (lib, "d3dx11.lib")
 #pragma comment (lib, "d3dx10.lib")
 
-/*struct VERTEX
-{
-	D3DXVECTOR3 pos;      // position
-	D3DXCOLOR Color;    // color
-};
-*/
-
 class ShaderBase
 {
 private:
